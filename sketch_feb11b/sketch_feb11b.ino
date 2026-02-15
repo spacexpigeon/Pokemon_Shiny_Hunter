fe61e8bb649a3b6e0cc5ff7448c62c58901183a2
Michał Rydzik <miskokakola@gmail.com>
2026-02-15 13:58:26 +0100

    poprawki

--- sketch_feb11b/sketch_feb11b.ino
+++ sketch_feb11b/sketch_feb11b.ino
@@ -3,13 +3,13 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-// --- TWOJE USTAWIENIA ---
+
 #define S0_GORA 150
 #define S0_DOL  475
 #define S1_GORA 400
 #define S1_DOL  150
 
-// Funkcja podtrzymująca (zostawiamy, bo działa)
+
 void czekajZPodtrzymaniem(int sekundy) {
   for (int i = 0; i < sekundy; i++) {
     pwm.setPWM(0, 0, S0_GORA); 
@@ -19,7 +19,7 @@ void czekajZPodtrzymaniem(int sekundy) {
 }
 
 void setup() {
-  Serial.begin(115200); // Komunikacja z PC
+  Serial.begin(115200); 
   pwm.begin();
   pwm.setPWMFreq(60);
 
@@ -35,7 +35,7 @@ void loop() {
   Serial.println("START_SEKWENCJI"); 
   czekajZPodtrzymaniem(6); 
 
-  // --- SERIA KLIKNIĘĆ PRZYCISKU A ---
+
   
   Serial.println("KLIK 1");
   pwm.setPWM(0, 0, S0_DOL); delay(500); pwm.setPWM(0, 0, S0_GORA);
@@ -56,21 +56,17 @@ void loop() {
   Serial.println("KLIK 5");
   pwm.setPWM(0, 0, S0_DOL); delay(500); pwm.setPWM(0, 0, S0_GORA);
   
-  // --- CZAS NA ZAŁADOWANIE WALKI ---
+
   Serial.println("Czekam na walkę (wydłużony czas)...");
-  
-  // ZMIANA: Było 14, jest 16. To daje +2 sekundy zapasu.
+ 
   czekajZPodtrzymaniem(16); 
 
-  // --- KOMUNIKACJA Z PC ---
-  
-  // 1. Wyczyść bufor
+
   while(Serial.available() > 0) { Serial.read(); }
 
-  // 2. Wyślij sygnał do Pythona
+
   Serial.println("SKANUJ"); 
 
-  // 3. Czekaj na odpowiedź z komputera
   Serial.println("Czekam na decyzję PC...");
   
   bool decyzjaOdebrana = false;
@@ -80,20 +76,20 @@ void loop() {
       char komenda = Serial.read();
       
       if (komenda == 'r') {
-        // --- SCENARIUSZ 1: ZWYKŁY HO-OH (RESET) ---
+
         Serial.println("Otrzymano RESET - to zwykły Pokemon.");
         
-        // Wykonaj reset
+
         pwm.setPWM(1, 0, S1_DOL); 
         delay(600); 
         pwm.setPWM(1, 0, S1_GORA); 
         
-        // Czekanie po resecie
+
         czekajZPodtrzymaniem(15); 
         decyzjaOdebrana = true; 
       }
       else if (komenda == 's') {
-        // --- SCENARIUSZ 2: SHINY! (STOP) ---
+
         Serial.println("SHINY!!! ZATRZYMUJĘ WSZYSTKO!");
         while(true) {
           pwm.setPWM(0, 0, S0_GORA);
